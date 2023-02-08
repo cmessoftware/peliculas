10ef3cdd531b9e6ebbdec0f483e0b8d09c085eeb
CMES Software <cmessotftware@gmail.com>
2023-02-07 21:05:31 -0300

    Refactorin para usar DI

--- Peliculas/DTOs/ActorDto.cs
+++ Peliculas/DTOs/ActorDto.cs
@@ -3,9 +3,13 @@
     public class ActorDto
     {
         public int Id { get; set; }
+
+        public ActorPeliculaRel ActorPeliculaRel { get; set; }
         public string Nombre { get; set; }
+
         public int Edad { get; set; }
+
         public string Pais { get; set; }
-        public bool EsPrincipal { get; set; }
+        public bool EsPrincipal { get; internal set; }
     }
 }
