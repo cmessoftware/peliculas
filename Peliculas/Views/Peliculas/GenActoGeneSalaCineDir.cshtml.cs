9493378ddba3fc840697a17071b41721f6236189
romanolucasandres <romanolucasandres@gmail.com>
2023-02-23 18:03:27 -0300

    Cambios

---
+++ Peliculas/Views/Peliculas/GenActoGeneSalaCineDir.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Peliculas.Views.Peliculas
+{
+    public class GenActoGeneSalaCineDirModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
